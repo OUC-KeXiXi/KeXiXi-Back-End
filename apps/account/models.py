5789043a09bb229eba6c195e4d2e436f8e1b1d65
刘阳 <249606097@qq.com>
2021-09-06 22:38:05 +0800

    feat: 用户模型

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class AccountRole:
+    Buyer = 0
+    Seller = 1
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=30, verbose_name='用户名', unique=True, null=False, blank=False)
+    password = models.CharField(max_length=100, verbose_name='密码', null=False, blank=False)
+    email = models.EmailField(verbose_name='邮箱', unique=True, null=False, blank=False)
+
+    role = models.CharField(max_length=1, verbose_name='角色',
+                            choices=((AccountRole.Buyer, '买家'), (AccountRole.Seller, '卖家')),
+                            null=False, blank=False)
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def info(self):
+        return AccountInfo.objects.filter(account=self)[0]
+
+
+class AccountInfo(models.Model):
+    account = models.ForeignKey('account.Account', on_delete=models.PROTECT, verbose_name='用户')
+    nickname = models.CharField(max_length=50, verbose_name='昵称', null=False, blank=True, default='')
+    avatar = models.ImageField(upload_to='media', verbose_name='头像', default='/media/default.png')
+
+    balance_integer = models.BigIntegerField(verbose_name='余额整数部分', default=0)
+    balance_decimal = models.IntegerField(verbose_name='余额小数部分', default=0)
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.account.username
+
+    def gain(self, integer, decimal):
+        self.balance_integer += integer
+
+        decimal_part = self.balance_decimal + decimal
+        self.balance_integer += decimal_part // 100
+        self.balance_decimal = decimal_part % 100
